12e26ec7c18f5252964d3bdd3abcd5e372d13190
cburgoin-dev <cristian.burgoin.dev@gmail.com>
2026-03-08 21:02:32 -0700

    "Minor fixes in Login and SignUp screens"

--- app/src/main/java/com/example/mainscreen/RegisterScreen.kt
+++ app/src/main/java/com/example/mainscreen/RegisterScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -36,6 +37,7 @@ fun RegisterScreen() {
         Box(
             modifier = Modifier
                 .fillMaxWidth(0.85f)
+                .fillMaxHeight(0.9f)
                 .background(
                     Color.White,
                     shape = RoundedCornerShape(40.dp)
